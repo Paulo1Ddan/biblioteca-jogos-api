ba6ff32f9b60f2ae8993bbed8f1d62accbdf13e2
Paulo Daniel <paulodaniel1360@gmail.com>
2025-07-17 21:23:29 -0300

    Biblioteca finalizada

--- app/Http/Controllers/GameLibraryController.php
+++ app/Http/Controllers/GameLibraryController.php
@@ -9,7 +9,7 @@ class GameLibraryController extends Controller
 {
     /**
      * @OA\Get(
-     *     path="/api/library/{$user_id}",
+     *     path="/api/library/{user_id}",
      *     tags={"Biblioteca"},
      *     summary="Lista todos os jogos do usuário na biblioteca",
      *     @OA\Response(
@@ -30,6 +30,32 @@ class GameLibraryController extends Controller
         return response()->json($user->games);
     }
 
+    /**
+     * @OA\Post(
+     *     path="/api/library/add",
+     *     tags={"Biblioteca"},
+     *     summary="Adiciona o jogo a biblioteca",
+     *     @OA\RequestBody(
+     *         required=true,
+     *         @OA\MediaType(
+     *             mediaType="multipart/form-data",
+     *             @OA\Schema(
+     *                 required={"user_id", "game_id"},
+     *                 @OA\Property(property="user_id", type="int", example="1"),
+     *                 @OA\Property(property="game_id", type="int", example="1")
+     *             )
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=200,
+     *         description="Jogo adicionado a biblioteca"
+     *     ),
+     *     @OA\Response(
+     *         response=400,
+     *         description="O jogo já está presente na biblioteca"
+     *     )
+     * )
+     */
     public function addGame(Request $request)
     {
         $rules = [
@@ -44,11 +70,50 @@ class GameLibraryController extends Controller
             "game_id.exists" => "Jogo inválido.",
         ];
 
+        $request->validate($rules, $messages);
+
         $user = User::find($request->user_id);
         if ($user->games()->where('game_id', $request->game_id)->exists()) {
             return response()->json(["message" => "O jogo já está presente na biblioteca"]);
         }
         $user->games()->attach($request->game_id);
         return response()->json(["message" => "O jogo foi adicionado a sua biblioteca"]);
+    }
+
+    /**
+     * @OA\Delete(
+     *     path="/api/library/remove",
+     *     tags={"Biblioteca"},
+     *     summary="Remove o jogo a biblioteca",
+     *     @OA\RequestBody(
+     *         required=true,
+     *         @OA\MediaType(
+     *             mediaType="multipart/form-data",
+     *             @OA\Schema(
+     *                 required={"user_id", "game_id", "_method"},
+     *                 @OA\Property(property="user_id", type="int", example="1"),
+     *                 @OA\Property(property="game_id", type="int", example="1"),
+     *                 @OA\Property(property="_method", type="string", example="delete")
+     *             )
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=200,
+     *         description="Jogo removido a biblioteca"
+     *     ),
+     *     @OA\Response(
+     *         response=400,
+     *         description="Esse jogo não está na sua lista"
+     *     )
+     * )
+     */
+    public function removeGame(Request $request)
+    {
+        $user = User::find($request->user_id);
+        if (!$user->games()->where('game_id', $request->game_id)->exists()) {
+            return response()->json(["message" => "Esse jogo não está na sua lista"]);
+        }
+        $user->games()->detach($request->game_id);
+        return response()->json(["message" => "O jogo foi removido da sua biblioteca"]);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -9,6 +9,7 @@ Route::apiResource("users", UserController::class);
 Route::apiResource("games", GameController::class);
 Route::get("/library/{user}", [GameLibraryController::class, "index"])->name("library");
 Route::post("/library/add", [GameLibraryController::class, "addGame"])->name("library.addGame");
+Route::delete("/library/remove", [GameLibraryController::class, "removeGame"])->name("library.removeGame");
 
 Route::get("/teste", function(){
     return response()->json(["message" => "Hello World!"]);
